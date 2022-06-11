ca0f7125cb7c278e506aee1d6afb6b0f6fee12b3
ByteFalcons <>
2022-06-11 15:27:37 +0530

    Added Operator precedence demo

---
+++ src/com/bytefalcons/coffeebean/basics/OperatorPrecedence.java
@@ -0,0 +1,25 @@
+package com.bytefalcons.coffeebean.basics;
+
+/**
+ * ============= OPERATOR PRECEDENCE ================
+ * 1) Unary Operators
+ *     ( [], x++, x-- )
+ *     ( ++x, --x, ~, ! )
+ *     ( new , <type> )
+ * 2) Binary Operators
+ * 3) Shift Operators
+ * 4) Comparison Operators
+ * 5) Equality Operator
+ * 6) Bitwise Operator ( &, ^, | )
+ * 7) Short Circuit Operators ( && , || )
+ * 8) Ternary Operator ( ?: )
+ * 9) Assignment Operator ( =, +=, -=, *= ... )
+ *
+ * ============= OPERAND EVALUATION =================
+ *
+ *
+ */
+public class OperatorPrecedence {
+
+
+}
